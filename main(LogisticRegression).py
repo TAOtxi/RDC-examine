15bcef78d4a64a11a456d983357ad67223ce625a
TAOtxi <1321075268@qq.com>
2023-04-02 19:50:04 +0800

    🔧调整preprocessing.py函数的返回形式 🔥合并main(LinearRegression).py和main(Normalequation).py

--- main(LogisticRegression).py
+++ main(LogisticRegression).py
@@ -9,17 +9,36 @@ from sklearn.linear_model import LogisticRegression as LR
 data = pd.read_csv('datasets/gender_classification.csv')
 prep = Preprocessing(data.values)
 prep.LabelEncoder([-1])
+
+# prep.zscore([1, 2])
+prep.minmax([1, 2])
+
 X_train, y_train, X_test, y_test = prep.split()
 
-for alpha in np.linspace(0.05, 2, 40):
+max = []
+fig, ax = plt.subplots()
+for alpha in np.linspace(0.01, 4, 100):
 
     LG = LogisticRegression(alpha=alpha)
-
     LG.fit(X_train, y_train)
 
-    pred = Evaluation(X_test, LG.theta, y_test, 0.5)
-    print(pred.accuracy(), '\t\t', alpha)
+    pred = Evaluation(X_test, LG.theta, y_test)
+    TPR, FPR = pred.ROC()
+    ax.plot(FPR, TPR)
+    # plt.show()
+    for threshold in np.linspace(0., 1., 200):
 
+        max.append([pred.accuracy(threshold), threshold, alpha])
+    # plt.plot(FPR, TPR)
+    # print(TPR, FPR)
+    # plt.title('alpha = {}'.format(alpha))
+
+plt.show()
+max = np.array(max)
+maxlist = np.argsort(max[:, 0])[::-1][:20]
+for i in max[maxlist]:
+    print('accuracy = {}\t\tthreshold = {}\t\talpha = {}'.format(i[0], i[1], i[2]))
+print("前20的决策边界的平均值", np.mean(max[maxlist][:, 1]))
 LR = LR()
 LR.fit(X_train, y_train.flatten())
 
--- tool/preprocessing.py
+++ tool/preprocessing.py
@@ -2,98 +2,96 @@ import numpy as np
 
 class Preprocessing:
 
-    def __init__(self, data):
-        """
-        :param data: np.ndarray (n_sample, n_feature) - 要处理的数据集
-        """
-        self.data = data
-
-    def minmax(self, columns):
+    def minmax(self, data, columns):
         """
         :description: 极差标准化
+        :param data: np.ndarray - 数据集
         :param columns: int or list - 要转换的列的序号
         :return: np.ndarray - 转换后的数据集
         """
 
-        max = np.max(self.data[:, columns], axis=0)
-        min = np.min(self.data[:, columns], axis=0)
+        max = np.max(data[:, columns], axis=0)
+        min = np.min(data[:, columns], axis=0)
 
-        self.data[:, columns] = (self.data[:, columns] - min) / (max - min)
-        return self.data
+        data[:, columns] = (data[:, columns] - min) / (max - min)
+        return data
 
-    def zscore(self, columns):
+    def zscore(self, data, columns):
         """
         :description: 标准差标准化
+        :param data: np.ndarray - 数据集
         :param columns: int or list - 要转换的列的序号
         :return: np.ndarray - 转换后的数据集
         """
 
-        mean = np.mean(self.data[:, columns], axis=0)
-        std = np.std(self.data[:, columns].astype(float), axis=0)
+        mean = np.mean(data[:, columns], axis=0)
+        std = np.std(data[:, columns].astype(float), axis=0)
 
-        self.data[:, columns] = (self.data[:, columns] - mean) / std
-        return self.data
+        data[:, columns] = (data[:, columns] - mean) / std
+        return data
 
-    def maxabs(self, columns):
+    def maxabs(self, data, columns):
         """
         :description: 极大值绝对值标准化
+        :param data: np.ndarray - 数据集
         :param columns: int or list - 要转换的列的序号
         :return: np.ndarray - 转换后的数据集
         """
 
-        maxabs = np.max(np.abs(self.data[:, columns]), axis=0)
+        maxabs = np.max(np.abs(data[:, columns]), axis=0)
 
-        self.data[:, columns] = self.data[:, columns] / maxabs
-        return self.data
+        data[:, columns] = data[:, columns] / maxabs
+        return data
 
-    def LabelEncoder(self, columns):
+    def LabelEncoder(self, data, columns):
         """
         :description: 序号编码
+        :param data: np.ndarray - 数据集
         :param columns: list - 要转换的列的序号
         :return: np.ndarray - 转换后的数据集
         """
 
         for column in columns:
-            type = np.unique(self.data[:, column])
+            type = np.unique(data[:, column])
             for index, value in enumerate(type):
-                self.data[:, column][self.data[:, column] == value] = index
+                data[:, column][data[:, column] == value] = index
 
-        return self.data
+        return data
 
-
-    def ZeroOneEncoder(self, columns):
+    def OneHotEncoder(self, data, columns):
         """
-        :description: 0-1 编码
+        :description: 独热编码
+        :param data: np.ndarray - 数据集
         :param columns: list - 要转换的列的序号
         :return: np.ndarray - 转换后的数据集
         """
 
         for column in columns:
-
-            type = np.unique(self.data[:, column])
+            type = np.unique(data[:, column])
 
             if len(type) <= 2:
-                self.data[:, column] = np.where(self.data[:, column] == type[0], 1, 0)
+                data[:, column] = np.where(data[:, column] == type[0], 1, 0)
             else:
                 for i in type:
-                    self.data = np.insert(self.data, column + 1, np.where(self.data[:, column] == i, 1, 0), axis=1)
-                self.data = np.delete(self.data, column, axis=1)
+                    data = np.insert(data, column + 1, np.where(data[:, column] == i, 1, 0), axis=1)
+                data = np.delete(data, column, axis=1)
 
-        return self.data
+        return data
 
-    def split(self, frac=0.7, seed=None):
+    def split(self, data, frac=0.7, seed=None):
         """
         :description: 划分训练集和测试集
+        :param data: np.ndarray - 数据集
         :param frac: float - 训练集所占比例（默认为0.7）
         :param seed: float - 随机种子（默认为None）
         :return: np.ndarray * 4 - 划分好的训练集X_train, 训练集标签y_train, 测试集X_test, 测试集标签y_test
         """
 
-        n_samples = self.data.shape[0]
+        n_samples = data.shape[0]
         np.random.seed(seed)
         random_index = np.random.permutation(range(n_samples))
-        train = self.data[random_index[:int(n_samples * frac)]].astype(np.float64)
-        test = self.data[random_index[int(n_samples * frac):]].astype(np.float64)
+        train = data[random_index[:int(n_samples * frac)]].astype(np.float64)
+        test = data[random_index[int(n_samples * frac):]].astype(np.float64)
 
         X_train = train[:, :-1]
         y_train = train[:, -1].reshape(-1, 1)
