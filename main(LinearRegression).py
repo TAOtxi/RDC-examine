a49dae7004739764f51cf5c5f18118793dd43f5a
TAOtxi <1321075268@qq.com>
2023-04-02 19:58:22 +0800

    合并后的线性回归main

--- main(LinearRegression).py
+++ main(LinearRegression).py
@@ -1,28 +1,30 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from tool.preprocessing import Preprocessing
 from tool.predict import Evaluation
 from algorithm.LinearRegression import MiniBGD
+from algorithm.Normalequation import Normalequation
 
-data = pd.read_csv('insurance.csv')
-prep = Preprocessing(data.values)
+data = pd.read_csv('datasets/insurance.csv')
+data = data.values
+prep = Preprocessing()
 
-prep.zscore([0, 2])
-prep.ZeroOneEncoder([1, 4, 5])
+data = prep.zscore(data, [0, 2])
+data = prep.OneHotEncoder(data, [1, 4, 5])
 
-X_train, y_train, X_test, y_test = prep.split(frac=0.7)
+X_train, y_train, X_test, y_test = prep.split(data)
 
-model_view = []
-temp = []
-for beta in np.linspace(0.01, 1, 30):
+# 梯度下降模型
+LR = MiniBGD(alpha=0.4, beta=1, tol=1e-4)
+LR.fit(X_train, y_train)
+pred_grad = Evaluation(X_test, LR.theta, y_test)
 
-    for alpha in np.linspace(0.01, 0.45, 30):
+# 最小二乘法模型
+NE = Normalequation()
+NE.fit(X_train, y_train)
+pred_NE = Evaluation(X_test, NE.theta, y_test)
 
-        LR = MiniBGD(alpha=alpha, beta=beta, tol=1e-4, maxiter=1e4)
-        LR.fit(X_train, y_train)
-        pred = Evaluation(X_test, LR.theta, y_test)
-        temp = [alpha, beta, LR.loss[-1], pred.R2()]
-        model_view.append(temp)
-model_view = pd.DataFrame(model_view, columns=['alpha', 'beta', 'loss', 'R2'])
-print(model_view)
+print('-'*15 + '梯度下降' + '-'*15)
+print('R2值%f， 平均方差%f' % (pred_grad.R2(), pred_grad.MSE()))
+print('-'*15 + '最小二乘法' + '-'*15)
+print('R2值%f， 平均方差%f' % (pred_NE.R2(), pred_NE.MSE()))
+
